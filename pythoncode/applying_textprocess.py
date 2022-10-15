8d70c9df071717e7348d32f27585de3d993ca330
Yasamin <71235733+yasaminrn@users.noreply.github.com>
2022-10-14 21:30:38 -0700

    Update applying_textprocess.py

--- pythoncode/applying_textprocess.py
+++ pythoncode/applying_textprocess.py
@@ -2,24 +2,16 @@
 #The pre-process is the first step to format the data for proper use of Natural language toolkit
 
 #importing utility libraries
-
 import re
 import numpy as np
 import pandas as pd
 
 #importing dataset
-
 DATASET_ENCODING = "ISO-8859-1"
-
 #File location and address
-
 #File names are taken automatically from interface company_list
 #For single use of applying_textprocess, can use the FILE_NAME as shown in exapmle below.
 #FILE_NAME = 'META.csv'
-
-
-# In[8]:
-
 
 #Defining column titles
 #For training/testing data, two columns of text and target are used
@@ -34,11 +26,8 @@ else:
     df = pd.read_csv(FILE_NAME, encoding=DATASET_ENCODING)
 
 #checking for Null values, in whole table and in text column.
-
 np.sum(df.isnull().any(axis=1))
-
 np.sum(df['text'].isnull())
-
 if re.search('train', FILE_NAME):
     data=df[['text','target']]
 elif re.search('test', FILE_NAME):
@@ -49,45 +38,34 @@ else:
 get_ipython().run_line_magic('run', 'text_process.ipynb')
 
 #changing all tweets to lower case
-
 data['text']
 
 #As we want to overwrite data frame disbaling the default warning for overwrite
-
 pd.options.mode.chained_assignment = None  # default='warn'
-
 data['text']=data['text'].str.lower()
 
 #applying the cleaning_stopwords function defined in text_process notebook
-
 data['text']=data['text'].apply(lambda text: cleaning_stopwords(text))
 
 #applying the cleaning_punctuations function defined in text_process notebook
-
 data['text']=data['text'].apply(lambda x:cleaning_punctuations(x))
 
 #applying the cleaning_repeating_char function defined in text_process notebook
-
 data['text']=data['text'].apply(lambda x: cleaning_repeating_char(x))
 
 #Applying cleaning_URLs function defined in text_process notebook
-
 data['text']=data['text'].apply(lambda x: cleaning_URLs(x))
 
 #applying the cleaning_numbers function defined in text_process notebook
-
 data['text']=data['text'].apply(lambda x: cleaning_numbers(x))
 
 #applying the word_tokenize function
-
 data['text']=data['text'].apply(word_tokenize)
 
 #applying stemming_on_text function defined in text_process notebook
-
 data['text']=data['text'].apply(lambda x: stemming_on_text(x))
 
 #applying lemmatizer_on_text function defined in text_process notebook
-
 data['text']=data['text'].apply(lambda x: lemmatizer_on_text(x))
 
 data['text']
